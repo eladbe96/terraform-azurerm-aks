60388df9bdf3121533c7a90a8b0faa883ae7de9e
Elad Ben-David <eladbe@checkpoint.com>
2023-07-24 16:47:14 +0300

    added my AKS config

--- examples/multiple_node_pools/main.tf
+++ examples/multiple_node_pools/main.tf
@@ -31,6 +31,52 @@ resource "azurerm_subnet" "test" {
   enforce_private_link_endpoint_network_policies = true
 }
 
+resource "azurerm_network_security_group" "eladSG_1" {
+  name                = "elad-nsg"
+  location            = var.location
+  resource_group_name = coalesce(var.resource_group_name, "${random_id.prefix.hex}-rg")
+
+  security_rule {
+    name                       = "AllowAll"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "AllowPort8080"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "AllowPort10250"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10250"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "eladSub" {
+  subnet_id                 = azurerm_subnet.test.id
+  network_security_group_id = azurerm_network_security_group.eladSG_1.id
+}
+
 locals {
   nodes = {
     for i in range(3) : "worker${i}" => {
@@ -42,15 +88,16 @@ locals {
   }
 }
 
-module "aks" {
-  source = "../.."
+module "aks_example_multiple_node_pools" {
+  source  = "Azure/aks/azurerm//examples/multiple_node_pools"
+  version = "7.2.0"
 
-  prefix                        = "prefix-${random_id.prefix.hex}"
-  resource_group_name           = local.resource_group.name
-  os_disk_size_gb               = 60
-  public_network_access_enabled = false
-  sku_tier                      = "Standard"
-  rbac_aad                      = false
-  vnet_subnet_id                = azurerm_subnet.test.id
-  node_pools                    = local.nodes
+#  prefix                        = "prefix-${random_id.prefix.hex}"
+#  resource_group_name           = local.resource_group.name
+#  os_disk_size_gb               = 60
+#  public_network_access_enabled = false
+#  sku_tier                      = "Standard"
+#  rbac_aad                      = false
+#  vnet_subnet_id                = azurerm_subnet.test.id
+#  node_pools                    = local.nodes
 }
